40183da4b5d490578c4c7cecc3bf66900dd608e7
Mike Grabowski <grabbou@gmail.com>
2015-11-17 21:04:05 +0100

    Generate default config in a separate func for readability sake

--- src/config.js
+++ src/config.js
@@ -2,10 +2,26 @@ var log = require('npmlog');
 var efs = require('./utils/fs');
 
 /**
- * Loads config for `rnpm` to use by projects.
+ * Generates default config from a package.json of the current project
  *
- * The default settings are always synced up with the latest React Native folder
+ * The generated settings are always synced up with the latest React Native folder
  * structure created by `react-native init <name>`.
+ *
+ */
+function generateDefaultConfig(pjson) {
+  return {
+    name: pjson.name,
+    ios: {
+      project: `./ios/${pjson.name}/${pjson.name}.xcodeproj`
+    },
+    android: {
+      project: `./android/app/build.gradle`
+    }
+  };
+}
+
+/**
+ * Loads config for `rnpm` to use by projects.
  *
  * In order to override default settings, simply mirror them under `rnpm` key in your
  * package.json.
@@ -20,15 +36,7 @@ function loadConfig() {
     return log.warn('EPACKAGEJSON', `Not found. Are you sure it's a React Native project?`);
   }
 
-  var defaultConfig = {
-    name: pjson.name,
-    ios: {
-      project: `./ios/${pjson.name}/${pjson.name}.xcodeproj`
-    },
-    android: {
-      project: `./android/app/build.gradle`
-    }
-  };
+  var defaultConfig = generateDefaultConfig(pjson);
 
   return pjson.rnpm
     ? Object.assign({}, defaultConfig, pjson.rnpm)
