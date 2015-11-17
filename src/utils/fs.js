c547deb4dda136f852cba8bc85a902921df663f7
Mike Grabowski <grabbou@gmail.com>
2015-11-17 21:07:34 +0100

    Docs & add in new method to fs

--- src/utils/fs.js
+++ src/utils/fs.js
@@ -1,18 +1,34 @@
 var fs = require('fs');
 var path = require('path');
 
-exports.loadFile = function loadFile(filePath) {
-  try {
-    return fs.readFileSync(path.join(process.cwd(), filePath));
-  } catch(err) {
-    return;
-  }
-}
-
+/**
+ * Tries to require given file in a try-catch block. If file is not present,
+ * null value is returend allowing other methods to handle errors gracefuly.
+ */
 exports.requireFile = function requireFile(filePath) {
   try {
     return require(path.join(process.cwd(), filePath));
   } catch(err) {
-    return;
+    return null;
   }
+}
+
+/**
+ * Tries to load given file in a try-catch block. For the detailed behaviour,
+ * see `requireFile` docs.
+ */
+exports.loadFile = function loadFile(filePath) {
+  try {
+    return fs.readFileSync(path.join(process.cwd(), filePath), {encoding: 'utf8'});
+  } catch(err) {
+    return null;
+  }
+}
+
+/**
+ * Returns an array of React Native dependencies, that is all npm packages
+ * that are subject to this plugin
+ */
+exports.loadDependencies = function loadDependencies() {
+ return [];
 }
