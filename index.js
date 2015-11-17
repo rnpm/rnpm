a1c28925c8f850b9b46fd2667f9ccb36c72db0a4
Mike Grabowski <grabbou@gmail.com>
2015-11-17 21:37:38 +0100

    Clarify inline comment

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ var cli = require('./src/init')();
 
 // do something with projects.ios && projects.android now
 // projects.ios.addDependency('');
+// use cli.actions to form commander
 console.log(cli);
