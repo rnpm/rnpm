68517b0445e393e3b6ad8eb97160034fd982d9cb
Mike Grabowski <grabbou@gmail.com>
2015-11-18 13:58:45 +0100

    Clarify commit

--- src/actions/update.js
+++ src/actions/update.js
@@ -1,5 +1,7 @@
-function updateProjects(cli) {
-  console.log('Done');
+function updateProjects(cli, program) {
+  // cli is our cli.actions, cli.projects and so on, so we have more flexibilty, but we can easily scope that to just `projects`.
+  // just want the cli in case there's gonna be some global config regarding actions, like cli.config.verbose etc.
+  console.log('Done', program.force);
 }
 
 module.exports = {
