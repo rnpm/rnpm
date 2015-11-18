4ea810cf6b31187842c3501fd62ddebcb808b83e
Mike Grabowski <grabbou@gmail.com>
2015-11-18 13:49:30 +0100

    Support flags

--- src/actions/update.js
+++ src/actions/update.js
@@ -4,5 +4,8 @@ function updateProjects(cli) {
 
 module.exports = {
   description: 'This action updates your project and links all native dependencies',
+  options: [
+    ['-f --force', 'Force linking of all dependencies. This removes everything and links it again']
+  ],
   run: updateProjects
 };
