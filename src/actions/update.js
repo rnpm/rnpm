663cc552b049153a8aab7f713975136e8cfc58cb
Mike Grabowski <grabbou@gmail.com>
2015-11-18 13:46:01 +0100

    Initial commit

---
+++ src/actions/update.js
@@ -0,0 +1,8 @@
+function updateProjects(cli) {
+  console.log('Done');
+}
+
+module.exports = {
+  description: 'This action updates your project and links all native dependencies',
+  run: updateProjects
+};
