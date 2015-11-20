0c53fab1868762638dbff3a2b6afc80f34aa836e
Kureev Alexey <kureev-mail@ya.ru>
2015-11-20 14:54:02 +0100

    Clean up Android patcher code

--- src/android/index.js
+++ src/android/index.js
@@ -1,184 +1,140 @@
 const fs = require('fs-extra');
 const path = require('path');
 const compose = require('lodash.flowright');
+const pjson = require('./package.json');
 
-const BUILD_GRADLE_PATH = path.join(
-  process.cwd(), 'android', 'app', 'build.gradle'
-);
+module.exports = function registerNativeAndroidModule(name) {
+  const SETTINGS_PATCH_PATTERN = `include ':app'`;
+  const BUILD_PATCH_PATTERN = `dependencies {`;
+  const MAIN_ACTIVITY_IMPORT_PATTERN = `import android.app.Activity;`;
+  const MAIN_ACTIVITY_PACKAGE_PATTERN = `.addPackage(new MainReactPackage())`;
 
-const SETTINGS_GRADLE_PATH = path.join(
-  process.cwd(), 'android', 'settings.gradle'
-);
+  const BUILD_GRADLE_PATH = path.join(
+    process.cwd(), 'android', 'app', 'build.gradle');
 
-const ASSETS_PATH = path.join(
-  process.cwd(), 'android', 'app', 'src', 'main', 'assets'
-);
+  const MODULE_DIR = path.join(
+    process.cwd(), 'node_modules', name);
 
-const composeMainActivityPath = (name) => path.join(
-  process.cwd(), 'android', 'app', 'src', 'main',
-  'java', 'com', name, 'MainActivity.java'
-);
+  const SETTINGS_GRADLE_PATH = path.join(
+    process.cwd(), 'android', 'settings.gradle');
 
-const SETTINGS_PATCH_PATTERN = `include ':app'`;
-const BUILD_PATCH_PATTERN = `dependencies {`;
-const MAIN_ACTIVITY_IMPORT_PATTERN = `import android.app.Activity;`;
-const MAIN_ACTIVITY_PACKAGE_PATTERN = `.addPackage(new MainReactPackage())`;
+  const ASSETS_PATH = path.join(
+    process.cwd(), 'android', 'app', 'src', 'main', 'assets');
 
-/**
- * Get module dir
- * @param  {String} name Name of the module
- * @return {String}      Path to the module inside node_modules folder
- */
-const getModuleDir = (name) =>
-  path.join(process.cwd(), 'node_modules', name);
+  const MAIN_ACTIVITY_PATH = path.join(
+    process.cwd(), 'android', 'app', 'src', 'main',
+    'java', 'com', pjson.name, 'MainActivity.java'
+  );
 
-/**
- * Get module's package.json content
- * @param  {String} name Name of the module
- * @return {Object}      Content of module's package.json
- */
-const getModulePackage = (name) =>
-  fs.readFileSync(path.join(getModuleDir(name), 'package.json'), 'utf8');
+  const modulePackageContent = fs.readFileSync(
+    path.join(MODULE_DIR, 'package.json'), 'utf8'
+  );
 
-/**
- * Get config from package.json's content
- * @param  {String} content Content of package.json
- * @return {Object}         Module's rnpm config
- */
-const getConfig = (content) => JSON.parse(content).rnpm;
+  /**
+   * Get config from package.json's content
+   * @param  {String} content Content of package.json
+   * @return {Object}         Module's rnpm config
+   */
+  const getConfig = (content) => JSON.parse(content).rnpm;
 
-/**
- * @param  {String} Name of the module
- * @return {Object} Android config for processing module
- */
-const getModuleAndroidConfig = compose(
-  (c) => c.android, getConfig, getModulePackage
-);
+  const readFile = (file) => () => fs.readFileSync(file, 'utf8');
+  const writeFile = (file, content) => content ?
+    fs.writeFileSync(file, content, 'utf8') :
+    (c) => fs.writeFileSync(file, c, 'utf8');
+  const replace = (scope, pattern, patch) =>
+    scope.replace(pattern, `${pattern}\n${patch}`);
 
-const readFile = (file) => () => fs.readFileSync(file, 'utf8');
-const writeFile = (file, content) => {
-  if (content) {
-    return fs.writeFileSync(file, content, 'utf8');
-  }
+  const writeSettingsGradle = (content) =>
+    fs.writeFileSync(SETTINGS_GRADLE_PATH, content, 'utf8');
+  const writeBuildGradle = (content) =>
+    fs.writeFileSync(BUILD_GRADLE_PATH, content, 'utf8');
 
-  return (c) => fs.writeFileSync(file, c, 'utf8');
-};
+  const SETTINGS_PATCH = `include ':react-native-vector-icons'\n` +
+    `project(':${name}').projectDir = new File(rootProject.projectDir, ` +
+    `'../node_modules/${name}/android')\n`;
 
-const writeSettingsGradle = (content) =>
-  fs.writeFileSync(SETTINGS_GRADLE_PATH, content, 'utf8');
-const writeBuildGradle = (content) =>
-  fs.writeFileSync(BUILD_GRADLE_PATH, content, 'utf8');
+  const getBuildPatch = () => `    compile project(':${name}')`;
 
-const replace = (scope, pattern, patch) =>
-  scope.replace(pattern, `${pattern}\n${patch}`);
+  const getImportPatch = (importPath) =>
+    `import ${importPath}`;
 
-const getSettingsPatch = (name) =>
-  `include ':react-native-vector-icons'\n` +
-  `project(':${name}').projectDir = ` +
-    `new File(rootProject.projectDir, '../node_modules/${name}/android')\n`;
+  const getPackagePatch = (instance) =>
+    `                .addPackage(${instance})`;
 
-const getBuildPatch = (name) =>
-  `    compile project(':${name}')`;
-
-const getImportPatch = (importPath) =>
-  `import ${importPath}`;
-
-const getPackagePatch = (instance) =>
-  `                .addPackage(${instance})`;
-
-/**
- * Make a project settings patcher
- * @param  {String}   name Name of the project we're going to include
- * @return {Function}      Patcher function
- */
-const makeProjectSettingsPatcher = (name) =>
   /**
    * Replace SETTINGS_PATCH_PATTERN by patch in the passed content
    * @param  {String} content Content of the Settings.gradle file
    * @return {String}         Patched content of Settings.gradle
    */
-  (content) => replace(content, SETTINGS_PATCH_PATTERN, getSettingsPatch(name));
+  const patchProjectSettings = (content) =>
+    replace(content, SETTINGS_PATCH_PATTERN, SETTINGS_PATCH);
 
-/**
- * Make a project settings patcher
- * @param  {String}   name Name of the project we're going to include
- * @return {Function}      Patcher function
- */
-const makeProjectBuildPatcher = (name) =>
   /**
    * Replace BUILD_PATCH_PATTERN by patch in the passed content
    * @param  {String} content Content of the Build.gradle file
    * @return {String}         Patched content of Build.gradle
    */
-  (content) => replace(content, BUILD_PATCH_PATTERN, getBuildPatch(name));
+  const patchProjectBuild = (content) =>
+    replace(content, BUILD_PATCH_PATTERN, getBuildPatch);
 
-/**
- * Make a MainActivity.java program patcher
- * @param  {String}   importPath Import path, e.g. com.oblador.vectoricons.VectorIconsPackage;
- * @param  {String}   instance   Code to instance a package, e.g. new VectorIconsPackage();
- * @return {Function}            Patcher function
- */
-const makeMainActivityPatcher = (importPath, instance) =>
-  (content) =>
-    replace(content, MAIN_ACTIVITY_IMPORT_PATTERN, getImportPatch(importPath)) &&
-    replace(content, MAIN_ACTIVITY_PACKAGE_PATTERN, getPackagePatch(instance));
+  /**
+   * Make a MainActivity.java program patcher
+   * @param  {String}   importPath Import path, e.g. com.oblador.vectoricons.VectorIconsPackage;
+   * @param  {String}   instance   Code to instance a package, e.g. new VectorIconsPackage();
+   * @return {Function}            Patcher function
+   */
+  const makeMainActivityPatcher = (importPath, instance) =>
+    (content) =>
+      replace(content, MAIN_ACTIVITY_IMPORT_PATTERN, getImportPatch(importPath)) &&
+      replace(content, MAIN_ACTIVITY_PACKAGE_PATTERN, getPackagePatch(instance));
 
-/**
- * @param  {String} name Name of the project we're going to include
- * @return {Function}    Settings.gradle patcher
- */
-const applySettingsGradlePatch = (name) =>
-  compose(
+  /**
+   * @return {Function}    Settings.gradle patcher
+   */
+  const applySettingsGradlePatch = compose(
     writeFile(SETTINGS_GRADLE_PATH),
-    makeProjectSettingsPatcher(name),
+    patchProjectSettings,
     readFile(SETTINGS_GRADLE_PATH)
   );
 
-/**
- * @param  {String} name Name of the project we're going to include
- * @return {Function}    Build.gradle patcher
- */
-const applyBuildGradlePatch = (name) =>
-  compose(
+  /**
+   * @return {Function}    Build.gradle patcher
+   */
+  const applyBuildGradlePatch = compose(
     writeFile(BUILD_GRADLE_PATH),
-    makeProjectBuildPatcher(name),
+    patchProjectBuild,
     readFile(BUILD_GRADLE_PATH)
   );
 
-/**
- * @param  {String}   importPath Import path, e.g. com.oblador.vectoricons.VectorIconsPackage;
- * @param  {String}   instance   Code to instance a package, e.g. new VectorIconsPackage();
- * @return {Function}            Patcher function
- */
-const applyMainActivityPatch = (name, importPath, instance) => {
-  const mainActivityPath = composeMainActivityPath(name);
+  /**
+   * @param  {String}   importPath Import path, e.g. com.oblador.vectoricons.VectorIconsPackage;
+   * @param  {String}   instance   Code to instance a package, e.g. new VectorIconsPackage();
+   * @return {Function}            Patcher function
+   */
+  const applyMainActivityPatch = (importPath, instance) =>
+    compose(
+      writeFile(MAIN_ACTIVITY_PATH),
+      makeMainActivityPatcher(importPath, instance),
+      readFile(MAIN_ACTIVITY_PATH)
+    );
 
-  return compose(
-    writeFile(mainActivityPath),
-    makeMainActivityPatcher(importPath, instance),
-    readFile(mainActivityPath)
-  );
-};
+  const copyAssets = (assets) => {
+    if (!assets) assets = [];
 
-const copyAssets = (name, assets) => {
-  if (!assets) assets = [];
+    return () => assets.forEach((asset) =>
+      fs.copySync(
+        path.join(MODULE_DIR, asset),
+        path.join(ASSETS_PATH, asset)
+      )
+    );
+  };
 
-  return () => assets.forEach((asset) =>
-    fs.copySync(
-      path.join(getModuleDir(name), asset),
-      path.join(ASSETS_PATH, asset)
-    )
-  );
-};
-
-module.exports = function registerNativeAndroidModule(name) {
-  const config = compose(getConfig, getModulePackage)(name);
-  const pjson = require('./package.json');
+  const config = getConfig(modulePackageContent);
 
   compose(
-    copyAssets(name, config.assets),
-    applySettingsGradlePatch(name),
-    applyBuildGradlePatch(name),
+    copyAssets(config.assets),
+    applySettingsGradlePatch,
+    applyBuildGradlePatch,
     applyMainActivityPatch(
       pjson.name,
       config.android.importPath,
