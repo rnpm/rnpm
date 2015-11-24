f893dc13d05eaa08e3b64aa3389af2290763186c
Kureev Alexey <kureev-mail@ya.ru>
2015-11-24 22:37:39 +0100

    Make Android patcher work

--- src/android/registerNativeModule.js
+++ src/android/registerNativeModule.js
@@ -1,39 +1,63 @@
 const fs = require('fs-extra');
-const efs = require('../utils/fs');
 const path = require('path');
+const xml = require('xmldoc');
 const compose = require('lodash.flowright');
-const utils = require('../utils');
+
+const BASE_PATH = path.join(process.cwd(), 'android');
+
+const manifest = fs.readFileSync(path.join(BASE_PATH, 'app', 'src', 'main', 'AndroidManifest.xml'));
+const packageName = new xml.XmlDocument(manifest).attr.package.split('.')[1];
 
 const SETTINGS_PATCH_PATTERN = `include ':app'`;
 const BUILD_PATCH_PATTERN = `dependencies {`;
 const MAIN_ACTIVITY_IMPORT_PATTERN = `import android.app.Activity;`;
 const MAIN_ACTIVITY_PACKAGE_PATTERN = `.addPackage(new MainReactPackage())`;
+const BUILD_GRADLE_PATH = path.join(BASE_PATH, 'app', 'build.gradle');
+const SETTINGS_GRADLE_PATH = path.join(BASE_PATH, 'settings.gradle');
+const ASSETS_PATH = path.join(BASE_PATH, 'app', 'src', 'main', 'assets');
+const MAIN_ACTIVITY_PATH = path.join(BASE_PATH, 'app', 'src', 'main', 'java',
+  'com', packageName, 'MainActivity.java');
 
-/**
- * Given package name, returns Java import declaration to be
- * inserted in the file
- */
-const getImportPatch = (importPath) => `import ${importPath}`;
+const readFile = (file) =>
+  () => fs.readFileSync(file, 'utf8');
 
-/**
- * Given package initialisation, returns Java .addPackage function call
- * to be attached in the MainActivity.java
- */
-const getPackagePatch = (instance) => `                .addPackage(${instance})`;
+const writeFile = (file, content) => content ?
+  fs.writeFileSync(file, content, 'utf8') :
+  (c) => fs.writeFileSync(file, c, 'utf8');
 
-module.exports = function registerNativeAndroidModule(name, dependencyConfig, projectConfig) {
-  const buildPatch = `    compile project(':${name}')`;
-  const settingsPatch = `include ':${name}'
-project(':${name}').projectDir = new File(rootProject.projectDir, \
-'../node_modules/${name}/android')`;
+const replace = (scope, pattern, patch) =>
+  scope.replace(pattern, `${pattern}\n${patch}`);
 
-  // Replace SETTINGS_PATCH_PATTERN by patch in the passed content
+module.exports = function registerNativeAndroidModule(name) {
+  const MODULE_DIR = path.join(process.cwd(), 'node_modules', name);
+  const BUILD_PATCH = `    compile project(':${name}')`;
+  const SETTINGS_PATCH = `include ':${name}'\n` +
+    `project(':${name}').projectDir = ` +
+    `new File(rootProject.projectDir, '../node_modules/${name}/android')`;
+
+  const config = require(path.join(MODULE_DIR, 'package.json')).rnpm;
+
+  const getImportPatch = (importPath) =>
+    `import ${importPath}`;
+
+  const getPackagePatch = (instance) =>
+    `                .addPackage(${instance})`;
+
+  /**
+   * Replace SETTINGS_PATCH_PATTERN by patch in the passed content
+   * @param  {String} content Content of the Settings.gradle file
+   * @return {String}         Patched content of Settings.gradle
+   */
   const patchProjectSettings = (content) =>
-    utils.replace(content, SETTINGS_PATCH_PATTERN, settingsPatch);
+    replace(content, SETTINGS_PATCH_PATTERN, SETTINGS_PATCH);
 
-  // Replace BUILD_PATCH_PATTERN by patch in the passed content
+  /**
+   * Replace BUILD_PATCH_PATTERN by patch in the passed content
+   * @param  {String} content Content of the Build.gradle file
+   * @return {String}         Patched content of Build.gradle
+   */
   const patchProjectBuild = (content) =>
-    utils.replace(content, BUILD_PATCH_PATTERN, buildPatch);
+    replace(content, BUILD_PATCH_PATTERN, BUILD_PATCH);
 
   /**
    * Make a MainActivity.java program patcher
@@ -43,8 +67,20 @@ project(':${name}').projectDir = new File(rootProject.projectDir, \
    */
   const makeMainActivityPatcher = (importPath, instance) =>
     (content) =>
-      utils.replace(content, MAIN_ACTIVITY_IMPORT_PATTERN, getImportPatch(importPath)) &&
-      utils.replace(content, MAIN_ACTIVITY_PACKAGE_PATTERN, getPackagePatch(instance));
+      replace(content, MAIN_ACTIVITY_IMPORT_PATTERN, getImportPatch(importPath)) &&
+      replace(content, MAIN_ACTIVITY_PACKAGE_PATTERN, getPackagePatch(instance));
+
+  const applySettingsGradlePatch = compose(
+    writeFile(SETTINGS_GRADLE_PATH),
+    patchProjectSettings,
+    readFile(SETTINGS_GRADLE_PATH)
+  );
+
+  const applyBuildGradlePatch = compose(
+    writeFile(BUILD_GRADLE_PATH),
+    patchProjectBuild,
+    readFile(BUILD_GRADLE_PATH)
+  );
 
   /**
    * @param  {String}   importPath Import path, e.g. com.oblador.vectoricons.VectorIconsPackage;
@@ -53,40 +89,32 @@ project(':${name}').projectDir = new File(rootProject.projectDir, \
    */
   const applyMainActivityPatch = (importPath, instance) =>
     compose(
-      efs.writeFile.bind(null, projectConfig.mainActivity),
+      writeFile(MAIN_ACTIVITY_PATH),
       makeMainActivityPatcher(importPath, instance),
-      efs.loadFile.bind(null, projectConfig.mainActivity)
+      readFile(MAIN_ACTIVITY_PATH)
     );
 
-  // Copy assets from dependencyConfig.folder to projectConfig.assetsFolder
+  /**
+   * Copy assets from MODULE_DIR to ASSETS_PATH
+   * @param  {Array} assets Array of assets specified in config
+   * @return {Function}
+   */
   const copyAssets = (assets) => {
     return () => (assets || []).forEach((asset) =>
       fs.copySync(
-        path.join(dependencyConfig._folder, asset),
-        path.join(projectConfig.assetsFolder, asset)
+        path.join(MODULE_DIR, asset),
+        path.join(ASSETS_PATH, asset)
       )
     );
   };
 
-  const applySettingsGradlePatch = compose(
-    efs.writeFile.bind(null, projectConfig.settings),
-    patchProjectSettings,
-    efs.loadFile.bind(null, projectConfig.settings)
-  );
-
-  const applyBuildGradlePatch = compose(
-    efs.writeFile.bind(null, projectConfig.project),
-    patchProjectBuild,
-    efs.loadFile.bind(null, projectConfig.project)
-  );
-
   compose(
-    copyAssets(dependencyConfig.assets),
+    copyAssets(config.assets),
     applySettingsGradlePatch,
     applyBuildGradlePatch,
     applyMainActivityPatch(
-      dependencyConfig.packageImportPath,
-      dependencyConfig.packageInstance
+      config.android.importPath,
+      config.android.instance
     )
   )();
 };
