021096b6fd7ead0650e0c6319ca58a62efa70878
Kureev Alexey <kureev-mail@ya.ru>
2015-11-26 23:18:21 +0100

    Improved actions detection algorythm

--- src/getActions.js
+++ src/getActions.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const flatten = require('lodash.flatten');
 const pjson = require(path.join(process.cwd(), 'package.json'));
 
 /**
@@ -9,14 +10,33 @@ const pjson = require(path.join(process.cwd(), 'package.json'));
 const isPlugin = (dependency) => !!~dependency.indexOf('rnpm-plugin-');
 
 /**
+ * Get default actions from rnpm's package.json
+ * @type {Array}
+ */
+const defaultActions = Object.keys(
+  require(path.join(__dirname, '..', 'package.json')).dependencies
+  ).filter(isPlugin);
+
+/**
  * Get plugin config
  * @param  {String} name Name of the plugin
  * @return {Object}      Plugin's config
  */
 const getPluginConfig = (name) =>
-  require(path.join(process.cwd(), 'node_modules', name, 'config.js'));
+  require(path.join(process.cwd(), 'node_modules', name));
+
+/**
+ * Compose a list of dependencies from default actions,
+ * package's dependencies & devDependencies
+ * @type {Array}
+ */
+const pluginsList = flatten([
+  defaultActions,
+  Object.keys(pjson.dependencies || []),
+  Object.keys(pjson.devDependencies || []),
+]);
 
 module.exports = () =>
-  (pjson.devDependencies || [])
+  pluginsList
     .filter(isPlugin)
     .map(getPluginConfig);
