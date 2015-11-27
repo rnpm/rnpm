14f6f8cb61fba1755946894001fff85f72fb2428
Kureev Alexey <kureev-mail@ya.ru>
2015-11-27 12:43:32 +0100

    getActions -> actions. Plugin detection logic has been improved: removed duplications

--- src/actions.js
+++ src/actions.js
@@ -10,6 +10,14 @@ const flatten = require('lodash.flatten');
 const isPlugin = (dependency) => !!~dependency.indexOf('rnpm-plugin-');
 
 /**
+ * Get plugin config
+ * @param  {String} name Name of the plugin
+ * @return {Object}      Plugin's config
+ */
+const getPluginConfig = cwd => name =>
+  require(path.join(cwd, 'node_modules', name));
+
+/**
  * Gets actions from package.json in the cwd given
  * @param {String} cwd Path to the folder to get the package.json from
  * @type  {Array}      Array of actions or an empty array if no package.json found
@@ -22,31 +30,22 @@ const getActions = (cwd) => {
   }
 
   const pjson = require(packagePath);
+  const deps = union(
+    Object.keys(pjson.dependencies || {}),
+    Object.keys(pjson.devDependencies || {})
+  );
 
-  return flatten(
-      Object.keys(pjson.dependencies || {}),
-      Object.keys(pjson.devDependencies || {})
-    )
-    .filter(isPlugin)
-    .map(getPluginConfig(cwd));
+  return deps.filter(isPlugin).map(getPluginConfig(cwd));
 };
-
-/**
- * Get plugin config
- * @param  {String} name Name of the plugin
- * @return {Object}      Plugin's config
- */
-const getPluginConfig = cwd => name =>
-  require(path.join(cwd, 'node_modules', name));
 
 /**
  * Compose a list of dependencies from
  * rnpm dependencies and current project dependencies
  * @type {Array}
  */
-const pluginsList = flatten([
+const actionsList = flatten([
   getActions(path.join(__dirname, '..')),
-  getActions(process.cwd())
+  getActions(process.cwd()),
 ]);
 
-module.exports = () => pluginsList;
+module.exports = unique(actionsList, 'name');
