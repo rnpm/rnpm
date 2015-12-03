2b8af9e79ee2e3f54be98710f3f245940dffee1a
Kureev Alexey <kureev-mail@ya.ru>
2015-12-03 12:55:55 +0100

    Added coments to findAndroidAppFolder

--- src/config/android/findAndroidAppFolder.js
+++ src/config/android/findAndroidAppFolder.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
+/**
+ * @param  {String} folder Folder to seek in
+ * @return {String}
+ */
 module.exports = function findAndroidAppFolder(folder) {
   if (!fs.existsSync(path.join(folder, 'android'))) {
     return null;
