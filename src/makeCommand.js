a905c45b6db5593856d7492e180300525cfdfbbb
Kureev Alexey <kureev-mail@ya.ru>
2016-01-18 17:14:13 +0100

    Add rnpm-hooks functionality to the rnpm core as commands

---
+++ src/makeCommand.js
@@ -0,0 +1,26 @@
+const spawn = require('child_process').spawn;
+const log = require('npmlog');
+
+module.exports = function makeCommand(command) {
+  return (cb) => {
+    if (!cb) {
+      cb(new Error(`You missed a callback function for the ${command} command`));
+    }
+
+    const args = command.split(' ');
+    const cmd = args.shift();
+
+    const commandProcess = spawn(cmd, args, {
+      stdio: 'inherit',
+      stdin: 'inherit',
+    });
+
+    commandProcess.on('close', function prelink(code) {
+      if (code) {
+        cb(new Error(`Error occured during executing "${command}" command`));
+      }
+
+      cb();
+    });
+  };
+};
