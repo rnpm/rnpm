4de9f945ae64175987276a43c5293ce92a3ce610
Kureev Alexey <kureev-mail@ya.ru>
2016-02-22 02:51:08 +0100

    Adjust makeCommand behavior + test coverage

--- test/makeCommand.spec.js
+++ test/makeCommand.spec.js
@@ -1,4 +1,11 @@
-jest.autoMockOff();
+var spawnError = false;
+
+jest.setMock('child_process', {
+  spawn: () => ({
+    on: (ev, cb) => cb(spawnError),
+  }),
+});
+jest.dontMock('../src/makeCommand');
 
 const makeCommand = require('../src/makeCommand');
 
@@ -13,14 +20,18 @@ describe('makeCommand', () => {
     expect(command).toThrow();
   });
 
-  it('should invoke a callback after command execution', (done) => {
+  it('should invoke a callback after command execution', () => {
     const spy = jest.genMockFunction();
-
     command(spy);
 
     setTimeout(() => {
-      expect(spy.calls.length).toBe(1);
-      done();
+      expect(spy.mock.calls.length).toBe(1);
     }, 1);
+  });
+
+  it('should throw an error if spawn ended up with error', () => {
+    spawnError = true;
+    const cb = jest.genMockFunction();
+    expect(() => command(cb)).toThrow();
   });
 });
